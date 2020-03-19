3a3d4ee780ecb0b2964c1c348012186a6019aa33
Austin T <austin.taranto@gmail.com>
2020-03-19 10:52:30 -0400

    fix nav update and fixed nav css

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
 
 function fixNav(e) {
-    console.log(e);
+    // console.log(e);
+    // console.log(topOfNav);
+    // console.log(topOfNav, window.scrollY);
+    if (window.scrollY >= topOfNav) {
+        document.body.classList.add("fixed-nav");
+    } else {
+        document.body.classList.remove("fixed-nav");
+    }
 }
 
 window.addEventListener("scroll", fixNav);
